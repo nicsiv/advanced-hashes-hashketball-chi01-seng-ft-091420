d401f6e87197e596b38d9fa5a5890b28bb7b0aae
Nicole Uyigue-Sivek <nicoleusivek@gmail.com>
2020-09-14 02:07:19 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -177,33 +177,19 @@ def player_numbers(input)
   return output
 end
 
-#def player_stats(name) 
-  #player_hash = {}
-  #game_hash.each do |team_position, team_data|
-   #team_data[:players].each do |player_name, player_data|
-     #if player_name == name 
-       #return player_data
-       #elsif player_name =! name
-      # return nil
-    #end
- #end
-#end
-#end
-
-def player_stats(input)
-  game_hash.each do |team, team_info|
-    team_info.each do |key, value|
-      if key == :players
-        value.each do |player|
-          if input == player[:player_name]
-            player.delete(:player_name) # having player name inside the hash was a bad idea!
-            return player
-          end
-        end
-      end
+def player_stats(name) 
+  player_hash = {}
+  game_hash.each do |team_position, team_data|
+   team_data[:players].each do |player_name, player_data|
+     if player_name == name 
+       return player_data
+       elsif player_name =! name
+      return nil
     end
-  end
+ end
 end
+end
+
 
 def big_shoe_rebounds
   big_shoe = 0
